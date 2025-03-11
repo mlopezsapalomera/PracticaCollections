8d349fd32dd0962b60eef5816e815e394e606061
Marcos <m.lopez5@sapalomera.cat>
2025-03-11 11:39:34 +0100

    Implementa el controlador y la vista del menú principal. Añado los menus en la vista y su logica en el controlador.

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,2 +1,53 @@
+import java.util.*;
+import Vista.Vista;
+
 public class Controlador {
+    public static Scanner scann = new Scanner(System.in);
+    public static void main(String[] args) {
+        menuPrincipalControlador();
+    }
+
+    public static void menuPrincipalControlador() {
+        int opcio;
+        Vista.menuPrincipal();
+        do {
+            opcio = scann.nextInt();
+            switch (opcio) {
+                case 1:
+                    menuIntroduirProductesControlador();
+                    break;
+                case 2:
+                    //pasarPerCaixa();
+                    break;
+                case 3:
+                    //mostrarCarret();
+                    break;
+                case 0:
+                    System.out.println("Fins aviat!");
+                    break;
+            }
+        } while (opcio != 0);
+    }
+
+    public static void menuIntroduirProductesControlador(){
+        int opcio;
+        Vista.menuIntroduirProducte();
+        do {
+            opcio = scann.nextInt();
+            switch (opcio) {
+                case 1:
+                    //introduirAliment();
+                    break;
+                case 2:
+                    //introduirTextil();
+                    break;
+                case 3:
+                    //introduirElectronica();
+                    break;
+                case 0:
+                    menuPrincipalControlador();
+                    break;
+            }
+        } while (opcio != 0);
+    }
 }
--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -1,4 +1,23 @@
 package Vista;
 
 public class Vista {
+    public static void menuPrincipal() {
+        System.out.println("------------");
+        System.out.println("-- INICI --- ");
+        System.out.println("------------");
+        System.out.println("1) Introducir Producte");
+        System.out.println("2) Passar per caixa");
+        System.out.println("3) Mostrar carret de compra");
+        System.out.println("0) Sortir");
+    }
+
+    public static void menuIntroduirProducte() {
+        System.out.println("---------------");
+        System.out.println("-- PRODUCTE --- ");
+        System.out.println("---------------");
+        System.out.println("1) Alimentacio");
+        System.out.println("2) Textil");
+        System.out.println("3) Electronica");
+        System.out.println("0) Tornar");
+    }
 }
