c36cf8f6b58222ff958cdac5a1084c3616563ced
Marcos López Medina <164485513+mlopezsapalomera@users.noreply.github.com>
2025-03-14 08:47:19 +0100

    Delete src directory

--- src/Model/Objectes/TextilComparator.java
+++
@@ -1,9 +0,0 @@
-package Model.Objectes;
-import java.util.Comparator;
-
-public class TextilComparator implements Comparator<Textil> {
-    @Override
-    public int compare(Textil t1, Textil t2) {
-        return t1.getComposicio().compareTo(t2.getComposicio());
-    }
-}
